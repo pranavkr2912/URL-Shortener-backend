8fada11ebf55a0a12a0ac0af32d96444a107df98
pranavkr2912 <112203901+pranavkr2912@users.noreply.github.com>
2026-01-16 01:47:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
-// test route
+
+const urlRoutes = require("./routes");
+app.use("/api", urlRoutes);
+
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
@@ -19,10 +21,7 @@ app.get("/", (req, res) => {
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Mongo error:", err));
-
+  .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
