35aa812dc2ef00f7b86959ffafc71450dd9393de
pranavkr2912 <112203901+pranavkr2912@users.noreply.github.com>
2026-01-16 01:49:13 +0530

    Update routes for URL shortening and error response
    Refactor URL shortening logic and improve error handling.

--- routes.js
+++ routes.js
@@ -1,49 +1,30 @@
-const express = require('express');
-const shortid = require('shortid');
-const mongoose = require('mongoose');
+const express = require("express");
+const shortid = require("shortid");
+const Url = require("./models");
 
 const router = express.Router();
 
-// schema
-const urlSchema = new mongoose.Schema({
-  longUrl: String,
-  shortCode: String
-});
 
-const Url = mongoose.model('Url', urlSchema);
-
-// test route
-router.get('/', (req, res) => {
-  res.send('Backend working');
-});
-
-// create short url
-router.post('/shorten', async (req, res) => {
+router.post("/shorten", async (req, res) => {
   const { longUrl } = req.body;
 
-  const shortCode = shortid.generate();
+  if (!longUrl) {
+    return res.status(400).json({ error: "URL required" });
+  }
+
+  const shortId = shortid.generate();
 
   const newUrl = new Url({
     longUrl,
-    shortCode
+    shortId,
   });
 
   await newUrl.save();
 
   res.json({
-    shortUrl: `http://localhost:5000/api/${shortCode}`
+    shortUrl: `${process.env.BASE_URL}/${shortId}`,
   });
 });
 
-// redirect
-router.get('/:code', async (req, res) => {
-  const url = await Url.findOne({ shortCode: req.params.code });
-
-  if (url) {
-    return res.redirect(url.longUrl);
-  } else {
-    return res.status(404).send('URL not found');
-  }
-});
-
 module.exports = router;
+
